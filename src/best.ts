9cb48a414ba88f581ef57f65d9a5fc66077d57aa
Kamil A <kamil.alekber@gmail.com>
2021-02-17 13:16:23 +0600

    chore: fix eslint type checking issue

--- src/best.ts
+++ src/best.ts
@@ -1,9 +1,10 @@
-const promise = new Promise((resolve) => {
+const promise: Promise<string> = new Promise((resolve) => {
   resolve("best, hello world");
 });
-let me: 123;
-promise.then(console.log);
+const me = 123;
+
+promise.then(console.log).catch(console.error);
 
 const t = "ttt";
 
-console.log(t);
+console.log(t, me);
